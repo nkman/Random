f0c7f8a89e67be7def823106ea45d3240b5008b6
Nkman <nkmann2@gmail.com>
2014-04-06 11:11:58 +0530

    unread message notification added

--- Fubot/app.js
+++ Fubot/app.js
@@ -46,6 +46,61 @@ fbgraph.fql(query_start, function(err, res){
 	});
 }
 
+var unseen_msg = function(){
+	fbgraph.fql("SELECT thread_id, unread, originator FROM thread WHERE folder_id=0 AND unseen=1", function(err, res){
+		var total = res["data"].length;
+		var i = 0;
+		var unread = 0;
+		while(i<total)
+		{
+			var so;
+			unread = res["data"][i]["unread"];  //defaq always remains the last result obtained.
+			var thread_id_ = res["data"][i]["thread_id"];
+			console.log(unread);
+			/*
+			var sender = res["data"][i]["originator"];
+		    fbgraph.get('/'+sender, function(err2,res2){
+				console.log(res2["first_name"]);
+				so = res2["first_name"];
+				fs.writeFile("./public/sender", so, function(err){});
+			});
+			*/
+			var t = function GET_MSG(unread,thread_id_){
+				fbgraph.fql("SELECT body, author_id FROM message WHERE thread_id="+thread_id_, function(err1, res1){
+					//console.log(res1["data"][0]);
+					var total_msg = res1["data"].length;
+					for(var j=total_msg-unread;j<total_msg;j++)
+					{
+						//console.log(j);
+						var msg = res1["data"][j]["body"];
+						var author_id = res1["data"][j]["author_id"];
+						/*
+						console.log(msg);
+						console.log(author_id);
+						*/
+						var Author_ = function(msg,author_id){
+							fbgraph.get("/"+author_id, function(err3,res3){
+							//console.log(unread);
+							console.log(res3["first_name"]);
+							console.log(msg);
+							notify.notify({title: res3["first_name"],message: msg});
+							});
+						}
+						Author_(msg,author_id);
+						/*
+						fs.readFile('./public/sender', 'utf8', function (err,data){
+							notify.notify({title: data,message: msg});
+						});
+						*/
+					}
+				});
+			}
+			t(unread,thread_id_);
+		i += 1;
+		}
+	});
+}
+
 /* Reads file(containing thread-id) and fetches messages */
 var q = function(){
 	var message_thread_id = [];
@@ -86,9 +141,10 @@ var notifications = function(){
 	});
 }
 
-p();
-poke();
-notifications();
+//p();
+//poke();
+//notifications();
+unseen_msg();
 
 app.get("/", function(req,res){
 	res.send(" responses and counting..");
