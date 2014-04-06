77fde552305c965d0dcfa243334797dd85100f37
Nkman <nkmann2@gmail.com>
2014-04-06 22:42:10 +0530

    bug-fixes

--- Fubot/app.js
+++ Fubot/app.js
@@ -20,6 +20,22 @@ var configs = require("./config/config.json");
 fbgraph.setAccessToken(configs.AccessToken);
 
 /*
+fbgraph.extendAccessToken({
+        "access_token":    configs.AccessToken, 
+        "client_id":      "428109280657827",
+         "client_secret":  "87b3807a42af976ea1ce717d372c3ead"
+    }, function (err, facebookRes) {
+       console.log(facebookRes);
+    });
+    
+fbgraph.extendAccessToken({
+        "client_id":      configs.id_
+      , "client_secret":  configs.secret
+    }, function (err, facebookRes) {
+       console.log(facebookRes);
+    });
+*/
+/*
 var query = {result : "SELECT message_id, viewer_id,  body FROM message WHERE thread_id = 310061185776886 LIMIT 100,200"};
 querys = ["select thread_id,message_count from thread where viewer_id = me() and folder_id = 0","select attachment, author_id, body, created_time, message_id, source, thread_id, viewer_id FROM message WHERE thread_id = 266956406748639 LIMIT 0,40"
 "",]
@@ -56,42 +72,20 @@ var unseen_msg = function(){
 			var so;
 			unread = res["data"][i]["unread"];  //defaq always remains the last result obtained.
 			var thread_id_ = res["data"][i]["thread_id"];
-			console.log(unread);
-			/*
-			var sender = res["data"][i]["originator"];
-		    fbgraph.get('/'+sender, function(err2,res2){
-				console.log(res2["first_name"]);
-				so = res2["first_name"];
-				fs.writeFile("./public/sender", so, function(err){});
-			});
-			*/
 			var t = function GET_MSG(unread,thread_id_){
 				fbgraph.fql("SELECT body, author_id FROM message WHERE thread_id="+thread_id_, function(err1, res1){
 					//console.log(res1["data"][0]);
 					var total_msg = res1["data"].length;
 					for(var j=total_msg-unread;j<total_msg;j++)
 					{
-						//console.log(j);
 						var msg = res1["data"][j]["body"];
 						var author_id = res1["data"][j]["author_id"];
-						/*
-						console.log(msg);
-						console.log(author_id);
-						*/
 						var Author_ = function(msg,author_id){
 							fbgraph.get("/"+author_id, function(err3,res3){
-							//console.log(unread);
-							console.log(res3["first_name"]);
-							console.log(msg);
 							notify.notify({title: res3["first_name"],message: msg});
 							});
 						}
 						Author_(msg,author_id);
-						/*
-						fs.readFile('./public/sender', 'utf8', function (err,data){
-							notify.notify({title: data,message: msg});
-						});
-						*/
 					}
 				});
 			}
@@ -141,11 +135,15 @@ var notifications = function(){
 	});
 }
 
-//p();
-//poke();
-//notifications();
-unseen_msg();
+cronJob = function(){
+	p();
+	poke();
+	notifications();
+	unseen_msg();
+	}
 
+cronJob();
+setInterval(cronJob, 1200000); //120 sec
 app.get("/", function(req,res){
 	res.send(" responses and counting..");
 });
---
+++ Fubot/fubot.sh
@@ -0,0 +1,3 @@
+echo "Starting Fubot"
+cd /home/nkman/Desktop/development/nkman/random/Fubot/
+node app.js
